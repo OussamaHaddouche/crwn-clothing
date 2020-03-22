88abed519ceee17515ce29be5cb37affd7c8802a
OussamaHaddouche <oussama1haddouche2@gmail.com>
2020-03-22 13:54:02 +0100

    Added authtification page withe signIn smart component and formInput and customButton dumb components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
 import { Route, Switch} from 'react-router-dom';
 import Header from './components/header/Header';
+import AuthentificationPage from './pages/authentification/AuthentificationPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signIn" component={AuthentificationPage} />
         </Switch>
      </>
   );
---
+++ src/pages/authentification/AuthentificationPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in';
+
+ const AuthentificationPage = () => {
+    return (
+        <div className="authentification">
+            <SignIn/>
+        </div>
+    )
+}
+
+export default AuthentificationPage
