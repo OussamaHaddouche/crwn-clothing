0e658032626f6c16671ff0ac9cafd74b44b49788
OussamaHaddouche <oussama1haddouche2@gmail.com>
2020-04-24 16:44:44 +0000

    added reselect package to reduce nbr of unnecessary re-renders

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -3,6 +3,7 @@ import CustomButton from '../custom-button/custom-button';
 import './cart-dropdown.scss';
 import { connect } from 'react-redux';
 import CartItem from '../cart-item/cart-item';
+import { selectCartItems } from '../../redux/cart/cart-selectors';
 
 
 const CartDropdown = ({cartItems}) => {
@@ -16,8 +17,8 @@ const CartDropdown = ({cartItems}) => {
     )
 }
 
-const mapStateToProps = ({cart:{cartItems}}) => ({
-    cartItems : cartItems
+const mapStateToProps = (state) => ({
+    cartItems : selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
